c75f45b308d26c738ab3b6ca040226fa1e58e49a
Eduardo Machado <eduardo.machado@maestro.local>
2021-11-06 02:46:11 -0300

    Correção campos cpf, cns e cep

--- cadastros/views.py
+++ cadastros/views.py
@@ -18,7 +18,7 @@ def valida_usuario(request):
     data_nascimento = request.POST.get('inputDataNasc')
     nome_mae = request.POST.get('inputNomeMae')
     nome_pai = request.POST.get('inputNomePai')
-    cep = request.POST.get('inputCEP').replace('-','')
+    cep = request.POST.get('inputCEP').replace('-', '')
     logradouro = request.POST.get('inputEndereco')
     num_endereco = request.POST.get('inputNumeroEndereco')
     complemento = request.POST.get('inputComplementoEndereco')
@@ -71,7 +71,7 @@ def pesquisa_usuarios(request):
             data_nascimento__icontains=data_nascimento,
             cpf__icontains=cpf,
             cns__icontains=cns,
-            nome_mae__icontains=nome_mae).order_by('-nome_usuario')
+            nome_mae__icontains=nome_mae).order_by('-nome_completo')
         print(usuario)
 
         return render(request, 'pesq_usuarios.html', {'usuarios': usuario})
